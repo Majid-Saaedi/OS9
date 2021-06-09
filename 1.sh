3505f65e51ddd88b1f7882e7b3a82255e09f7485
Majid-Saaedi <majidsaedi378.ms@gmail.com>
2021-06-09 17:25:21 +0430

    OS9_Majid-Saaedi

---
+++ 1.sh
@@ -0,0 +1,8 @@
+chown -v username:groupname filename
+chown -c username:groupname filename
+chown -R username:groupname filename
+gpasswd -r groupname
+gpasswd -d username groupname
+gpasswd -a username groupname
+gpasswd -A username groupname
+gpasswd -M username groupname
